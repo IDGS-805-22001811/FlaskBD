e314326179e27cd3bfc80a8c32dc9100fb9436d5
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-02-27 19:05:42 -0600

    layout

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+from config import DevelopmentConfig
+from models import db
+from models import Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
 
 @app.route("/")
 @app.route("/index")
@@ -8,4 +20,8 @@ def index():
 	return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    csrf.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run()
