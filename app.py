b5458f419da61a7213a7423ef0697c79edd8341d
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-03-04 18:54:33 -0600

    Agregar

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from config import DevelopmentConfig
 from models import db
 from models import Alumnos
+import forms
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -14,10 +15,36 @@ csrf=CSRFProtect()
 def page_not_found(e):
 	return render_template('404.html'),404
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form=forms.UserForm2(request.form)
+    alumno=Alumnos.query.all()#select * from alumnos
+    return render_template("index.html",form=create_form,alumno=alumno)
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        nom=alum1.nombre
+        ape=alum1.apaterno
+        email=alum1.email
+    return render_template("detalles.html",form=create_form,nombre=nom,ape=ape,email=email)
+
+@app.route("/Alumnos1",methods=['GET','POST'])
+def Alumnos1():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='POST':
+        alum=Alumnos(nombre=create_form.nombre.data,
+                     apaterno=create_form.apaterno.data,
+                     email=create_form.email.data)
+        #insert alumnos() vaules ()
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('Alumnos1.html',form=create_form)
 
 if __name__ == '__main__':
     csrf.init_app(app)
