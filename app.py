53377555380f8abb8ae307df66012227fdd48c57
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-03-06 19:41:39 -0600

    ABC completo

--- app.py
+++ app.py
@@ -46,6 +46,48 @@ def Alumnos1():
         return redirect(url_for('index'))
     return render_template('Alumnos1.html',form=create_form)
 
+@app.route("/modificar",methods=['GET','POST'])
+def modificar():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        #select * from alumnos where id == id
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=str.rstrip(alum1.nombre)
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.id=id
+        alum1.nombre=str.rstrip(create_form.nombre.data)
+        alum1.apaterno=create_form.apaterno.data
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('modificar.html',form=create_form)
+
+@app.route('/eliminar',methods=['GET','POST'])
+def eliminar():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        #Select * from alumnos where id==id
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=str.rstrip(alum1.nombre)
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum = Alumnos.query.get(id)
+        #delete from alumnos where id =id
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('eliminar.html',form=create_form)
+
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
